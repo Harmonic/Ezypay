ca696503dd098539f0bc05229f515489b1531273
Craig Harman <craig@harmonicnewmedia.com>
2019-06-11 14:32:01 +0800

    Fixed token date in fake

--- src/Testing/EzypayFake.php
+++ src/Testing/EzypayFake.php
@@ -729,7 +729,7 @@ class EzypayFake {
         $tokenData = json_decode($tokenDataFile, true);
 
         $now = Carbon::now();
-        $tokenIsExpired = ($now->gte(new Carbon($tokenData['expiration']['date'])));
+        $tokenIsExpired = ($now->gte(new Carbon($tokenData['expiration'])));
 
         if ($tokenIsExpired) {
             $tokenData = $this->requestToken($tokenData['refresh_token']);
